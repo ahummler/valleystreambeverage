94a0d267c717bcb6b36d607a20bdb9568eb1cb80
Arthur <phillysuperbowl@gmail.com>
2015-11-02 11:49:22 -0500

    added expander to faqgit statusgit status

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,64 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-// $(document).ready(function() {
+
+$(document).on('page:change', function() {
+
+	var expanderTrigger = document.getElementById("js-expander-trigger");
+    var expanderContent = document.getElementById("js-expander-content");
+    var expanderTrigger1 = document.getElementById("js-expander-trigger-1");
+    var expanderContent1 = document.getElementById("js-expander-content-1");
+    var expanderTrigger2 = document.getElementById("js-expander-trigger-2");
+    var expanderContent2 = document.getElementById("js-expander-content-2");
+    var expanderTrigger3 = document.getElementById("js-expander-trigger-3");
+    var expanderContent3 = document.getElementById("js-expander-content-3");
+    var expanderTrigger4 = document.getElementById("js-expander-trigger-4");
+    var expanderContent4 = document.getElementById("js-expander-content-4");
+    var expanderTrigger5 = document.getElementById("js-expander-trigger-5");
+    var expanderContent5 = document.getElementById("js-expander-content-5");
+    var expanderTrigger6 = document.getElementById("js-expander-trigger-6");
+    var expanderContent6 = document.getElementById("js-expander-content-6");
+    var expanderTrigger7 = document.getElementById("js-expander-trigger-7");
+    var expanderContent7 = document.getElementById("js-expander-content-7");
+
+	  $('#js-expander-trigger').click(function(){
+	  $(this).toggleClass("expander-hidden");
+	  console.log('works');
+
+	  $('#js-expander-trigger-1').click(function(){
+	  $(this).toggleClass("expander-hidden");
+	  console.log('works')
+
+	  $('#js-expander-trigger-2').click(function(){
+	  $(this).toggleClass("expander-hidden");
+	  console.log('works')
+
+	  $('#js-expander-trigger-3').click(function(){
+	  $(this).toggleClass("expander-hidden");
+	  console.log('works')
+
+	  $('#js-expander-trigger-4').click(function(){
+	  $(this).toggleClass("expander-hidden");
+	  console.log('works')
+
+	  $('#js-expander-trigger-5').click(function(){
+	  $(this).toggleClass("expander-hidden");
+	  console.log('works')
+
+	  $('#js-expander-trigger-6').click(function(){
+	  $(this).toggleClass("expander-hidden");
+	  console.log('works')
+
+	  $('#js-expander-trigger-7').click(function(){
+	  $(this).toggleClass("expander-hidden");
+	  console.log('works')
+		 });
+		});
+	  });
+	});
+   });
+  });
+ });
 //   var menuToggle = $('#js-mobile-menu').unbind();
 //   $('#js-navigation-menu').removeClass("show");
 
@@ -26,4 +83,4 @@
 //       }
 //     });
 //   });
-// });
+});
